371728f88c1232d1b791c07e7e3a549d029c2e0e
Orchard <orchardyz@outlook.com>
2025-03-22 00:00:31 +0800

    feat: support custom templates

--- src/extension.ts
+++ src/extension.ts
@@ -1,15 +1,12 @@
 import * as vscode from 'vscode';
 import { register as registerCreateTemplate } from './createTemplate/register';
 import { register as registerQuickJump } from './quickJump/register';
-import { register as registerCreateCustomTemplate } from './createCustomTemplate/register';
 
 export function activate(context: vscode.ExtensionContext) {
   console.log('Congratulations, your extension \'cbd-tools\' is now active!');
 
-  // 注册命令：创建模板
+  // 注册命令：创建模板（包含内置模板和自定义模板）
   registerCreateTemplate(context);
-  // 注册命令：创建自定义模板
-  registerCreateCustomTemplate(context);
   // 注册命令：快速跳转
   registerQuickJump(context);
 }
